27b0506cfc7895d94f4e6fd4f514f38822436cad
stollander33 <stollander33@users.noreply.github.com>
2022-12-19 23:04:26 +0000

    deploy: 89937160e9d62b3162bb576196bd522f173857ff

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev42+gd2580d3',
+    VERSION: '0.1.dev43+g8993716',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
